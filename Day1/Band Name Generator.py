6ca08709c8d6c8d772a16f982d844cd11ca92f32
Alex Kadiyski <alex_kadiyski2003@abv.bg>
2024-10-27 00:19:54 +0300

    Day 1-5 Uploaded
    Day 1-5 Uploaded

---
+++ Day1/Band Name Generator.py
@@ -0,0 +1,4 @@
+print("Hello! This is the Band Name Generator!")
+city = input("To proceed with generating your band name, what is the name of the city you grew up in?\n")
+pet = input("And finally, what is the name of your pet?\n")
+print("Your band name could be: " + city + " " + pet)
