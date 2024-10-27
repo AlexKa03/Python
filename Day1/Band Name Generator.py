4c1d6bea45850bebf2a3c7cb16e19b150a144af5
Alex Kadiyski <alex_kadiyski2003@abv.bg>
2024-10-27 17:16:36 +0200

    Day 1
    Day 1

--- Day1/Band Name Generator.py
+++ Day1/Band Name Generator.py
@@ -1,3 +1,5 @@
+# Band Name Generator
+
 print("Hello! This is the Band Name Generator!")
 city = input("To proceed with generating your band name, what is the name of the city you grew up in?\n")
 pet = input("And finally, what is the name of your pet?\n")
